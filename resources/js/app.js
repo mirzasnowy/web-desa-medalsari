978eb209b510b366baf087444096d154d5019a00
Muhammad Mirza <2210631170085@student.unsika.ac.id>
2025-07-06 23:58:56 +0700

    fix frontend for prod

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,2 @@
 import './bootstrap';
+import './desa-medalsari.js';
--- resources/views/admin/wisata_desas/show_public.blade.php
+++ resources/views/admin/wisata_desas/show_public.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $wisataDesa->nama }} - Detail Wisata Desa</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/desa-medalsari.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
     
